769bfbdd8cb6d1c3f0f11d2b66c76dd53d78fbab
PratapPawar <pratappawar1997@gmail.com>
2021-03-29 19:52:43 +0530

    Added: Monthly Employee Wage Calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,19 +2,20 @@
 		WagePerHr=20
 		isFullTime=1
 		isPartTime=2
+		DayInMonth=20
 		empCheck=$((RANDOM%3))
-		case $empCheck in
-				$isFullTime )
-				  				  empHr=8								
-                           ;;
-
-				$isPartTime )
-								  empHr=4
-									;;
-			           	*)
-					        	  empHr=0
-					            ;;				
-		 esac
-		 Salary=$(( WagePerHr*empHr ))
-       echo "Daily Employee Salary = "$Salary
+case $empCheck in
+		$isFullTime )
+			    empHr=8								
+                 ;;
+		$isPartTime )
+				 empHr=4
+					 ;;
+		*)
+			    empHr=0
+					 ;;				
+esac 
+Salary=$(( WagePerHr*empHr ))
+SalaryPerMonth=$(($Salary*$DayInMonth))
+echo "Monthly Employee Salary = " $SalaryPerMonth
 
