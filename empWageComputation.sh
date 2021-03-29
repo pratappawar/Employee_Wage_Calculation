4e1c83511fd2117dce53daa500e33d47473f7d39
PratapPawar <pratappawar1997@gmail.com>
2021-03-29 20:41:56 +0530

    Refactor: Monthly Employee Wage Calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,21 +1,21 @@
 #! /bin/bash 
-		WagePerHr=20
-		isFullTime=1
-		isPartTime=2
-		DayInMonth=20
-		empCheck=$((RANDOM%3))
-case $empCheck in
-		$isFullTime )
-			    empHr=8								
-                 ;;
-		$isPartTime )
-				 empHr=4
-					 ;;
+	WagePerHr=20
+	isFullTime=1
+	isPartTime=2
+	DayInMonth=20
+	empCheck=$((RANDOM%3))
+   case $empCheck in
+	$isFullTime )
+		empHr=8								
+			;;
+	$isPartTime )
+		empHr=4
+			;;
 		*)
-			    empHr=0
-					 ;;				
-esac 
-Salary=$(( WagePerHr*empHr ))
-SalaryPerMonth=$(($Salary*$DayInMonth))
-echo "Monthly Employee Salary = " $SalaryPerMonth
+		  empHr=0
+		   ;;				
+	esac 
+	Salary=$(( WagePerHr*empHr ))
+	SalaryPerMonth=$(($Salary*$DayInMonth))
+	echo "Monthly Employee Salary = " $SalaryPerMonth
 
